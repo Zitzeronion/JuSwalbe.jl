420b8b6879d92a8deb9100d7eedfbc166e0c213a
Zitzeronion <physiknerd@gmail.com>
2020-04-29 16:32:03 +0200

    More coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using Test
 # Pkg.test("JuSwalbe", test_args=["call"])
 @testset "JuSwalbe.jl" begin
     include("call.jl")
+    include("marcoscopic_tpys.jl")
     include("equilibriumcalculation.jl")
 end
