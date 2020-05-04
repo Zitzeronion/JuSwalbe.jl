5a8dec657fc0d6eeb4761980926fcec87927bc3d
Zitzeronion <physiknerd@gmail.com>
2020-05-04 18:16:46 +0200

    Some 2d calculations

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # JuSwalbe
 
 [![](https://img.shields.io/badge/docs-stable-blue.svg)](https://Zitzeronion.github.io/JuSwalbe.jl/stable)
-[![](https://img.shields.io/badge/docs-dev-blue.svg)](https://Zitzeronion.io/JuSwalbe.jl/dev)
+[![](https://img.shields.io/badge/docs-dev-blue.svg)](https://Zitzeronion.github.io/JuSwalbe.jl/dev)
 [![Build Status](https://travis-ci.com/Zitzeronion/JuSwalbe.svg?branch=master)](https://travis-ci.com/Zitzeronion/JuSwalbe)
 [![Build Status](https://ci.appveyor.com/api/projects/status/github/Zitzeronion/JuSwalbe?svg=true)](https://ci.appveyor.com/project/Zitzeronion/JuSwalbe)
 [![Codecov](https://codecov.io/gh/Zitzeronion/JuSwalbe.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/Zitzeronion/JuSwalbe.jl)
--- src/equilibriumcalculation.jl
+++ src/equilibriumcalculation.jl
@@ -8,13 +8,16 @@ As the expansion is made around the equilibrium, lattice Boltzmann always assume
 Therefore the equilibrium is calculated from macroscopic quantities, i.e. height `.height` and velocity `.velocity`.
 There are plenty of ways to calculate them I mainly the ones derived in Eq.(13) of [Study of the 1D lattice Boltzmann shallow water equation and its coupling to build a canal network.](https://www.sciencedirect.com/science/article/pii/S0021999110003372)
 
-# Math
+# Math:
 Defining equations in one spatial dimensions are expressed in the following way
 ``\\begin{align*} 
 f_0^{eq} &=  h - \\frac{1}{2v^2}gh^2 - \\frac{1}{v^2}hu^2 \\ 
 f_1^{eq} &=  \\frac{1}{4v^2}gh^2 + \\frac{1}{2v}hu + \\frac{1}{2v^2}hu^2 \\
 f_2^{eq} &=  \\frac{1}{4v^2}gh^2 - \\frac{1}{2v}hu + \\frac{1}{2v^2}hu^2 
 \\end{align*}``
+
+# Example
+TBD
 """
 function calc_equilibrium_distribution(mom::macroquant64_1d, gravity=0)
     len = length(mom.height)
@@ -53,3 +56,36 @@ function calc_equilibrium_distribution(mom::macroquant32_1d, gravity=0)
     end
     return equi_dist
 end
+
+function calc_equilibrium_distribution(mom::macroquant64_2d, gravity=0)
+    width = size(mom.height,1)
+    thick = size(mom.height,2)
+
+    v = 1.0
+    vsquare = v^2
+
+    weights = [4/9 1/9 1/9 1/9 1/9 1/36 1/36 1/36 1/36]
+    lattice_vel = [0 0; 1 0; 0 1; -1 0; 0 -1; 1 1; -1 1; -1 -1; 1 -1]
+
+    equi_dist = dist64_2d(zeros(Float64,(width, thick)), 
+                          zeros(Float64,(width, thick)), 
+                          zeros(Float64,(width, thick)),
+                          zeros(Float64,(width, thick)),
+                          zeros(Float64,(width, thick)),
+                          zeros(Float64,(width, thick)),
+                          zeros(Float64,(width, thick)),
+                          zeros(Float64,(width, thick)),
+                          zeros(Float64,(width, thick)))
+    
+    udotu = velocitysquared(mom)
+    equidict = struct2dict(equi_dist)
+
+    equi_dist.f0 = mom.height .- weights[1] .* mom.height .*(15.0/2.0 * gravity .* mom.height - 3.0/2.0 .* udotu)
+
+end
+
+function velocitysquared(mom::macroquant64_2d)
+    velsquared = mom.velocity.x.^2 + mom.velocity.y.^2
+    return velsquared
+end
+
