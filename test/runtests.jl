bbd59f542d10622cde862ac90b69e91cbb183eda
Zitzeronion <physiknerd@gmail.com>
2020-05-17 18:56:31 +0200

    reactivated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,14 @@ using JuSwalbe
 using Test, DrWatson, DelimitedFiles, Images
 
 @testset "JuSwalbe.jl" begin
-    # include("call.jl")
-    # include("macroscopic_typs.jl")
-    # include("distribution_types.jl")
-    # include("readinput.jl")
-    # include("equilibriumcalculation.jl")
-    # include("calculatepressure.jl")
-    # include("calcmoments.jl")
-    # include("forcing.jl")
+    include("call.jl")
+    include("macroscopic_typs.jl")
+    include("distribution_types.jl")
+    include("readinput.jl")
+    include("equilibriumcalculation.jl")
+    include("calculatepressure.jl")
+    include("calcmoments.jl")
+    include("forcing.jl")
     include("collide.jl")
 end
 
