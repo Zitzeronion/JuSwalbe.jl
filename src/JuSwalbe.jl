894382b36abe57b1198f211ca7f8b5f85be28c33
Zitzeronion <physiknerd@gmail.com>
2020-05-25 10:32:59 +0200

    Added Revise.jl, quality of life change

--- src/JuSwalbe.jl
+++ src/JuSwalbe.jl
@@ -1,28 +1,29 @@
 module JuSwalbe
 
-using Pkg
-using DelimitedFiles, JSON, DrWatson, Images, Parameters, BSON, Glob
+    using Pkg
+    using DelimitedFiles, JSON, DrWatson, Images, Parameters, BSON, Glob
+    using Revise
 
-include("minimalsetup.jl")
-include("macroscopic_typs.jl")
-include("distribution_types.jl")
-include("readinput.jl")
-include("equilibriumcalculation.jl")
-include("calculatepressure.jl")
-include("calcmoments.jl")
-include("forcing.jl")
-include("collide.jl")
-include("readwritedata.jl")
+    include("minimalsetup.jl")
+    include("macroscopic_typs.jl")
+    include("distribution_types.jl")
+    include("readinput.jl")
+    include("equilibriumcalculation.jl")
+    include("calculatepressure.jl")
+    include("calcmoments.jl")
+    include("forcing.jl")
+    include("collide.jl")
+    include("readwritedata.jl")
 
-# IO related
-export readinput, findargument, minimalsetup1d, minimalsetup2d, simplemoment2d, simpleTwovector, simpledistD2Q9
-# File IO
-export savecheckpoint, savecheckpoints, loadcheckpoint, height2file, velocity2file, velocityandheight2file
-# Pressure related 
-export Δh, Π, pressure, ∇p
-# Distributions functions and moments
-export calc_equilibrium_distribution, calculatemoments, dist2array, collisionBGK, streamdistperiodic!
-# Forces
-export computeslip
+    # IO related
+    export readinput, findargument, minimalsetup1d, minimalsetup2d, simplemoment2d, simpleTwovector, simpledistD2Q9
+    # File IO
+    export savecheckpoint, savecheckpoints, loadcheckpoint, height2file, velocity2file, velocityandheight2file
+    # Pressure related 
+    export Δh, Π, pressure, ∇p
+    # Distributions functions and moments
+    export calc_equilibrium_distribution, calculatemoments, dist2array, collisionBGK, streamdistperiodic!
+    # Forces
+    export computeslip
 
 end # module
